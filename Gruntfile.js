4f0a1c605a76f99cdf80f11d1f73c0c772fb050b
Dlani Mendes <dlanileonardo@gmail.com>
2013-08-18 05:42:28 -0300

    Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+/*jshint node: true */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        qunit: {
+            all: ['test/index.html']
+        },
+        jshint: {
+            files: [
+                'Gruntfile.js',
+                'jquery.live.search.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.license %> */\n'
+            },
+            build: {
+                files: {
+                    'build/jquery.live.search-<%= pkg.version %>.min.js': 'jquery.live.search.js'
+                }
+            }
+        },
+        watch: {
+            files: [
+                'jquery.live.search.js',
+                'test/tests.js'
+            ],
+            tasks: 'default'
+        },
+        compare_size: {
+            files: [
+                'build/jquery.live.search-<%= pkg.version %>.min.js',
+                'jquery.live.search.js'
+            ],
+            options: {
+                compress: {
+                    gz: function (fileContents) {
+                        return require('gzip-js').zip(fileContents, {}).length;
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-compare-size');
+
+    grunt.registerTask('default', ['jshint', 'qunit', 'uglify', 'compare_size']);
+    grunt.registerTask('ci', ['jshint', 'qunit']);
+};
